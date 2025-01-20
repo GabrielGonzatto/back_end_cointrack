d39e21466f7c94c8e012df692aeac21ee0559e0f
GabrielGonzatto <gabegonzatto@gmail.com>
2025-01-20 14:04:20 -0300

    Criando o projeto

---
+++ src/test/java/BackEnd_CoinTrack/BackEndCoinTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package BackEnd_CoinTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCoinTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
